a4f63bbd19cf8cf025e40e1657f68a35ba3dc634
noQils <daffaaqilmahmud@gmail.com>
2024-11-13 14:52:14 +0700

    finished tutorial 8

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,5 @@
-from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.http import JsonResponse
-from django.contrib.auth import logout as auth_logout
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 import json
